472444d27f85c9d8e53b16c8a421416bcb3d322c
feliphe <dias.rodrigues.feliphe@gmail.com>
2021-12-05 16:11:00 -0300

    changePort

--- build/index.js
+++ build/index.js
@@ -10,10 +10,10 @@ dotenv_1.default.config();
 const app = express_1.default();
 app.use(express_1.default.json());
 app.use('/', userRouter_1.default);
-const server = app.listen(3000, () => {
+const server = app.listen(process.env.port || 3000, () => {
     if (server) {
         const address = server.address();
-        console.log(`Server is running at http:/localhost:${address.port}`);
+        console.log(`Server is running at http://localhost:${address.port}`);
     }
     else {
         console.log(`Failed to start the server`);
