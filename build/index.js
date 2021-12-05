95a4712a2f66b28adf93ec1b4440831ae840d60a
feliphe <dias.rodrigues.feliphe@gmail.com>
2021-12-05 18:08:53 -0300

    add_cors

--- build/index.js
+++ build/index.js
@@ -5,9 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const userRouter_1 = __importDefault(require("./route/userRouter"));
 dotenv_1.default.config();
 const app = express_1.default();
+app.use(cors_1.default());
 app.use(express_1.default.json());
 app.use('/', userRouter_1.default);
 const server = app.listen(process.env.PORT || 3000, () => {
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import cors from 'cors';
 import { AddressInfo } from 'net';
 import userRouter from './route/userRouter';
 
@@ -8,6 +9,7 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/', userRouter);
 
